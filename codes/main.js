30d787b9650037902c428387ba61628e78b8424e
MohammadJouza <m.jozaa@yahoo.com>
2023-02-06 07:55:25 +0300

    finish coding

--- codes/main.js
+++ codes/main.js
@@ -3,22 +3,42 @@ const listOfIcons = ["automation.png", "tasks.png", "user.png"];
 const tabIcon = $("#tab-icon");
 const title = $("title");
 const body = $("#body");
-const icons = $("#icons");
+const iconsDiv = $("#icons");
+
+iconsDiv.css({
+  display: "flex",
+});
 
 listOfIcons.forEach((elem, i) => {
-  const newIcon = $(`<button>${elem}</button>`);
+  const newIconDiv = $(`<button></button>`);
+  const newIconImg = $(`<img/>`);
+  const newIconTitle = $(`<span>${elem.slice(0, elem.length - 4)}</span>`);
+  newIconDiv.append(newIconImg);
+  newIconDiv.append(newIconTitle);
 
-  newIcon.css({
-    "background-color": "red",
-    margin: "5px",
+  newIconDiv.css({
+    // "background-color": "red",
+    margin: "10px",
+    // border: "2px solid red",
+    display: "flex",
+    "font-size": "25px",
+    "align-items": "center",
+    "font-weight": "bold",
   });
 
-  newIcon.on("click", function () {
-    title.text(elem);
+  newIconImg.css({
+    width: "35px",
+    "margin-right": "5px",
+  });
+
+  newIconImg.attr("src", `./${elem}`);
+
+  newIconDiv.on("click", function () {
+    title.text(elem.slice(0, elem.length - 4));
     tabIcon.attr("href", `./${elem}`);
   });
 
-  icons.append(newIcon);
+  iconsDiv.append(newIconDiv);
 });
 
 body.append(listOfIcons);

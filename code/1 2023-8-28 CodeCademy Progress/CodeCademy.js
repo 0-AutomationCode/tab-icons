a349e343c5b09528d4e09761488e0e8381107947
Mohammad Jouza <m.jozaa@yahoo.com>
2023-09-02 22:40:18 +0300

    new icons

---
+++ code/1 2023-8-28 CodeCademy Progress/CodeCademy.js
@@ -0,0 +1,126 @@
+let t = 'hey';
+
+let array1 = [
+  104, 116, 116, 112, 115, 58, 47, 47, 101, 110, 103, 105, 110, 101, 101, 114,
+  105, 110, 103, 45, 97, 112, 112, 108, 105, 99, 97, 116, 105, 111, 110, 46, 98,
+  114, 105, 116, 101, 99, 111, 114, 101, 46, 99, 111, 109, 47, 113, 117, 105,
+  122, 47, 115, 97, 97, 115, 100, 97, 115, 100, 108, 102, 108, 102, 108, 115,
+];
+
+// hints: map, fromCharCode, replace, regex
+function BriteCore(a) {
+  let b = a.map(function (char) {
+    return String.fromCharCode(char);
+  });
+  return b.join('');
+}
+BriteCore(array1);
+/*
+let fernet = require('fernet')
+let secret = new fernet.Secret('TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM=')
+let message = 'gAAAAABcQN64sYj_4mLG24C4qFtegzqpb7pKYaWzY5Tu0RxfFx0_AANvGEAYXlKroFhm9-WQ58KKlkKu4q-ByfBcGT1sHC6MDc9453BY-uxH9GnsLRMvrgv6kv-loKQHzyjmd5oV8aRzwvuXQMgyGhd6-07REKctKLliHLOvzqmjYHZW0qduO0aX_CYN2hL09WWakpbxBdH0'
+let token = new fernet.Token({ secret: secret, token: message, ttl: 0 })
+token.decode()
+*/
+
+// facebook message private account
+
+//*[@id="jsc_c_c"]/div
+//*[@id="jsc_c_c"]/div
+/*
+const nodeList = document.querySelectorAll('[id="jsc_c_c"]');
+
+const identifiers = Array.from(nodeList).map(node =>
+  ({
+    id: node.dataset.id,
+    bt: node.dataset.bt
+  }));
+
+console.log(identifiers);
+*/
+
+location.href = 'https://www.messenger.com/t/100010010163161';
+
+user_1 = 100000748271533;
+user_2 = 100010010163161;
+user_3 = 100005465677327;
+
+function processAjaxData(response, urlPath) {
+  document.getElementById('content').innerHTML = response.html;
+  document.title = response.pageTitle;
+  window.history.pushState(
+    { html: response.html, pageTitle: response.pageTitle },
+    '',
+    urlPath
+  );
+
+
+
+
+
+
+}
+
+function processAjaxData_2(urlPath) {
+  document.getElementById('content').innerHTML = document.querySelector('body')
+  window.history.pushState(
+    { html: response.html, pageTitle: 'asd' },
+    '',
+    urlPath
+  );
+}
+
+
+let usersUrl_1 = [];
+let usersUrl_2 = [];
+function jouza() {
+  const nodeList = document.querySelector('[id="jsc_c_c"]');
+  const children = nodeList.children[0].children;
+  const arr = Array.from(children).slice(0, -1);
+
+  arr.forEach((elem, i) => {
+    // div.divs[0].divs[0].a[0].href
+    let newUserURL = elem.children[0].children[0].children[0].href;
+    // console.log('I:', i);
+    console.log('URL:', newUserURL);
+
+    // usersUrl_1.push(newUserURL);
+    usersUrl_2.push(newUserURL);
+  });
+}
+jouza();
+
+
+ let jouza= "https://www.messenger.com/t/100000530752897"
+ let wafaa="https://www.messenger.com/t/100033507311682"
+
+users=[
+  "https://www.messenger.com/t/100000748271533/",
+  "https://www.messenger.com/t/100010010163161/",
+  "https://www.messenger.com/t/100005465677327/",
+  "https://www.messenger.com/t/100004152334435/",
+  "https://www.messenger.com/t/100001578196400/",
+  "https://www.messenger.com/t/102375644701783/",
+  "https://www.messenger.com/t/100009851775587/",
+  "https://www.messenger.com/t/100023621232767/",
+  "https://www.messenger.com/t/1165936655/",
+  "https://www.messenger.com/t/100015891797581/",
+  "https://www.messenger.com/t/100003286731066/",
+  "https://www.messenger.com/t/100003226523558/",
+  "https://www.messenger.com/t/100059409589360/",
+  "https://www.messenger.com/t/100011085374311/",
+  "https://www.messenger.com/t/100002615502526/",
+  "https://www.messenger.com/t/100016586074184/",
+  "https://www.messenger.com/t/100005105534584/",
+  "https://www.messenger.com/t/100009041781424/",
+  "https://www.messenger.com/t/100003730039321/",
+  "https://www.messenger.com/t/729011299/",
+  "https://www.messenger.com/t/100004664497340/",
+  "https://www.messenger.com/t/100004854815645/",
+  "https://www.messenger.com/t/100005216946133/",
+  "https://www.messenger.com/t/100015573432202/",
+  "https://www.messenger.com/t/100003998893631/",
+  "https://www.messenger.com/t/100003828217784/",
+  "https://www.messenger.com/t/582240298/"
+]
+

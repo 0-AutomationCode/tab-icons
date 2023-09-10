c94a97780b4318cfdcdc63c7260e3f4cc2575154
Mohammad Jouza <m.jozaa@yahoo.com>
2023-09-10 22:14:30 +0300

    logos YT

---
+++ code/WA console code new Messages/main.js
@@ -0,0 +1,10 @@
+const titleText = "2023-9-10 9 AM";
+const title = $(`title`);
+title.text(titleText);
+
+// ! ===================
+
+console.log("2023-9-10");
+
+
+
---
+++ code/main.js
@@ -0,0 +1,5 @@
+console.log("2023-9-10");
+
+const titleText = "2023-9-10 9 AM";
+const title = $(`title`);
+title.text(titleText);

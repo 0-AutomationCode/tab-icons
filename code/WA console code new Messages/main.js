44686d5d0200198900798fb2d5182edcc2ee974c
Mohammad Jouza <m.jozaa@yahoo.com>
2023-09-13 18:38:33 +0300

    copy numbers (not in order)

--- code/WA console code new Messages/main.js
+++ code/WA console code new Messages/main.js
@@ -1,19 +1,62 @@
-const titleText = "2023-9-13 5 PM";
+const titleText = "9-13 5 PM | ";
 const title = $(`title`);
-title.text(titleText);
+title.text(titleText + title.text());
 
 // ! ===================
 
 console.log(titleText);
 
-const entireChat = $(`#pane-side`);
+const mainDiv = $(`#pane-side`);
 
-const AllNewMessages = entireChat.map((chat) => {
+const entireChat = mainDiv.children().children().children().children();
+
+let allNewMessagesNumbers = ``;
+
+const allNewMessages = entireChat.map((i, oneChat) => {
+  // console.log(oneChat);
+  const [phoneNumber, time] = $(
+    $($(oneChat).children().children().children().children()[1]).children()[0]
+  ).children();
+  const phoneNumberArray = $(phoneNumber).text().split(" ");
+  timeText = $(time).text();
+
+  let phoneNumberText = "";
+
+  if (phoneNumberArray[0] === "+962") {
+    phoneNumberText =
+      phoneNumberArray[1][0] +
+      phoneNumberArray[phoneNumberArray.length - 2] +
+      phoneNumberArray[phoneNumberArray.length - 1];
+  } else {
+    // console.log(phoneNumberArray);
+    phoneNumberText = phoneNumberArray.join("").split("\n").join("").slice(1);
+  }
+  allNewMessagesNumbers =
+    allNewMessagesNumbers +
+    `
+  ${phoneNumberText}`;
+
+  phoneNumberText = phoneNumberText;
   const newMessage = {
-    phoneNumber: ``,
-    time: ``,
+    phoneNumber: phoneNumberText,
+    time: timeText,
   };
   return newMessage;
 });
 
-console.log("ALLNEWMESSAGES: ", AllNewMessages);
+console.log("allNewMessages: ", allNewMessages);
+
+console.log("allNewMessagesNumbers: ", allNewMessagesNumbers);
+
+//  ! Test ===================
+
+const testChat = $(
+  $(
+    $(entireChat[2]).children().children().children().children()[1]
+  ).children()[0]
+).children();
+
+const [pn, t] = testChat;
+
+const textPhoneNumber = "+962 7\n                                    7739 6184";
+//  ! Test ===================

9e8b8550fea5fc90346c2943016cf5706270ca68
Mohammad Jouza <m.jozaa@yahoo.com>
2023-10-18 23:34:34 +0300

    code

---
+++ Z MERAKI/Github/HasRepo.js
@@ -0,0 +1,99 @@
+
+/**
+ * isAMember check if a student is member of MERAKI ORG
+ * @param {string} [o] - A optional string param
+ * @param {string} [d=DefaultValue] - A optional string param
+ * @param {string} studentGitHubUsername - GitHub Username.
+ * @param {string} token - the token for C# MERAKI ORG.
+ * @param {string} cohortORG - C# MERAKI ORG for example MERAKI-Academy-Cohort-9.
+ * @param {string} REQUEST_TYPE - by default is get.
+ * @return {boolean} true or false depends on if exist as a member or not.
+ * @customfunction
+ */
+
+
+
+
+
+
+
+
+const GitHubMainAPI = "https://api.github.com";
+
+/**
+ * isAMember check if a student is member of MERAKI ORG
+ * @param {string} [o] - A optional string param
+ * @param {string} [d=DefaultValue] - A optional string param
+ * @param {string} studentGitHubUsername - GitHub Username.
+ * @param {string} token - the token for C# MERAKI ORG.
+ * @param {string} cohortORG - C# MERAKI ORG for example MERAKI-Academy-Cohort-9.
+ * @param {string} REQUEST_TYPE - by default is get.
+ * @return {boolean} true or false depends on if exist as a member or not.
+ * @customfunction
+ */
+
+const isAMember = (
+  studentGitHubUsername,
+  token,
+  cohortORG,
+  REQUEST_TYPE = "get"
+) => {
+  // console.log("TOKEN:", token);
+
+  var options = {
+    method: REQUEST_TYPE,
+    headers: { Authorization: `Bearer ${token}` },
+    per_page: 100,
+    // 'payload' : formData,
+    muteHttpExceptions: true,
+  };
+
+  const endpoint = `${GitHubMainAPI}/orgs/${cohortORG}/members/${studentGitHubUsername}`;
+  // console.log("URL:", endpoint);
+
+  const res = UrlFetchApp.fetch(endpoint, options);
+  console.log("RES:", res);
+  console.log("STATUS:", res.getResponseCode());
+  // Logger.log(res.getContentText());
+
+  let result = res.getResponseCode() == 204 ? true : false;
+  console.log("RESULT:", result);
+
+  return result;
+
+  /*
+  try {
+    const res = UrlFetchApp.fetch(endpoint, options);
+    console.log("c", res);
+    console.log("c", res.getResponseCode());
+    Logger.log(res.getContentText());
+
+    result = res.getResponseCode() == 204 && true;
+    // console.log("z", result);
+  } catch (error) {
+    console.error(error);
+    result = false;
+    // Expected output: ReferenceError: nonExistentFunction is not defined
+    // (Note: the exact output may be browser-dependent)
+  }
+  */
+};
+
+// ================== TEST Section
+
+const token_c9 =
+  "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";
+
+const c9ORG = "MERAKI-Academy-Cohort-9";
+
+let testStudents = "MERAKI-Academy";
+testStudents = "Alaaalnseirat";
+testStudents = "AyaAlomarii";
+testStudents = "Huda";
+testStudents = "SondosAlrifae";
+testStudents = "SondosAlrifae";
+testStudents = "MohammadJouza";
+
+isAMember(testStudents, token_c9, c9ORG);
+
+// ! - start section 2
--- Z MERAKI/Test/main.js
+++ Z MERAKI/Test/main.js
@@ -1,8 +1,91 @@
-console.log('DATA: ',data)
+console.log("DATA: ", data);
+
+
+console.log("DATA2: ", data2);
+
+data.legnth;
+data2.legnth;
+
+
+
+
+const repos={
+
+}
+
+data2.forEach((e,i)=>{
+  repos[e.name]=e
+console.log(e.name)
+})
+
+
+console.log("DATA2: ", repos);
+
+
+
+oneRepo={
+  "id": 702414839,
+  "node_id": "R_kgDOKd3_9w",
+  "name": "S1L01_Introduction_To_JS",
+  "full_name": "C9-AyaAlomarii/S1L01_Introduction_To_JS",
+  "private": true,
+
+  "owner": {
+      "login": "C9-AyaAlomarii",
+      "id": 147243372,
+      "node_id": "O_kgDOCMbBbA",
+  },
+
+  "description": null,
+  "fork": false,
+  "created_at": "2023-10-09T09:19:35Z",
+  "updated_at": "2023-10-09T09:23:48Z",
+  "pushed_at": "2023-10-09T09:23:44Z",
+  "homepage": null,
+  "size": 2,
+  "stargazers_count": 0,
+  "watchers_count": 0,
+  "language": "JavaScript",
+  "has_issues": true,
+  "has_projects": true,
+  "has_downloads": true,
+  "has_wiki": false,
+  "has_pages": false,
+  "has_discussions": false,
+  "forks_count": 0,
+  "mirror_url": null,
+  "archived": false,
+  "disabled": false,
+  "open_issues_count": 0,
+  "license": null,
+  "allow_forking": false,
+  "is_template": false,
+  "web_commit_signoff_required": false,
+  "topics": [],
+  "visibility": "private",
+  "forks": 0,
+  "open_issues": 0,
+  "watchers": 0,
+  "default_branch": "main",
+  "permissions": {
+      "admin": true,
+      "maintain": true,
+      "push": true,
+      "triage": true,
+      "pull": true
+  }
+}
 
 
 
 
 
-data.legnth
+
+
+
+
+
+
+
+
 

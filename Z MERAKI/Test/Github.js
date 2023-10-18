62f9d0dc551e47ef1998bcc600d079aaa19dd8ac
Mohammad Jouza <m.jozaa@yahoo.com>
2023-10-18 15:43:03 +0300

    is member work 100%

--- Z MERAKI/Test/Github.js
+++ Z MERAKI/Test/Github.js
@@ -1,3 +1,9 @@
+const doneFunc = () => {
+  console.log("DONE");
+};
+
+// !- is the student a member of MERAKI Organization
+
 /*
 const getAllMembers = (endpoint, token, REQUEST_TYPE = "get") => {
   console.log("a", token);
@@ -25,36 +31,63 @@ getAllMembers(endpoint_c9, token_c9);
 
 const GitHubMainAPI = "https://api.github.com";
 
+/**
+ * isAMember check if a student is member of MERAKI ORG
+ * @param {string} [o] - A optional string param
+ * @param {string} [d=DefaultValue] - A optional string param
+ * @param {string} studentGitHubUsername - GitHub Username.
+ * @param {string} token - the token for C# MERAKI ORG.
+ * @param {string} cohortORG - C# MERAKI ORG for example MERAKI-Academy-Cohort-9.
+ * @param {string} REQUEST_TYPE - by default is get.
+ * @return {boolean} true or false depends on if exist as a member or not.
+ * @customfunction
+ */
+
 const isAMember = (
   studentGitHubUsername,
   token,
   cohortORG,
   REQUEST_TYPE = "get"
 ) => {
-  // console.log("a", token);
+  // console.log("TOKEN:", token);
 
   var options = {
     method: REQUEST_TYPE,
     headers: { Authorization: `Bearer ${token}` },
     per_page: 100,
-    // 'payload' : formData
+    // 'payload' : formData,
+    muteHttpExceptions: true,
   };
 
   const endpoint = `${GitHubMainAPI}/orgs/${cohortORG}/members/${studentGitHubUsername}`;
-  // console.log("b", endpoint);
+  // console.log("URL:", endpoint);
+
   const res = UrlFetchApp.fetch(endpoint, options);
+  console.log("RES:", res);
+  console.log("STATUS:", res.getResponseCode());
+  // Logger.log(res.getContentText());
 
-  // console.log("c", res);
-  // console.log("c", res.getResponseCode());
-  Logger.log(res.getContentText());
+  let result = res.getResponseCode() == 204 ? true : false;
+  console.log("RESULT:", result);
 
-  const result = res.getResponseCode() == 204 ? true : false;
-  // console.log("z", result);
   return result;
-};
 
-const doneFunc = () => {
-  console.log("DONE");
+  /*
+  try {
+    const res = UrlFetchApp.fetch(endpoint, options);
+    console.log("c", res);
+    console.log("c", res.getResponseCode());
+    Logger.log(res.getContentText());
+
+    result = res.getResponseCode() == 204 && true;
+    // console.log("z", result);
+  } catch (error) {
+    console.error(error);
+    result = false;
+    // Expected output: ReferenceError: nonExistentFunction is not defined
+    // (Note: the exact output may be browser-dependent)
+  }
+  */
 };
 
 // ================== TEST Section
@@ -62,13 +95,16 @@ const doneFunc = () => {
 const token_c9 =
   "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";
 
-let testStudents = "MERAKI-Academy";
-testStudents = "Huda";
-testStudents = "Alaaalnseirat";
-testStudents = "AyaAlomarii";
-
 const c9ORG = "MERAKI-Academy-Cohort-9";
 
-// isAMember(testStudents, token_c9, c9ORG);
+let testStudents = "MERAKI-Academy";
+testStudents = "Alaaalnseirat";
+testStudents = "AyaAlomarii";
+testStudents = "Huda";
+testStudents = "SondosAlrifae";
+testStudents = "SondosAlrifae";
+testStudents = "MohammadJouza";
+
+isAMember(testStudents, token_c9, c9ORG);
 
 // ! - start section 2

794c397cff365a80aac9403b779875f01bf8ecff
Mohammad Jouza <m.jozaa@yahoo.com>
2023-10-18 15:16:06 +0300

    done get member or not v03

--- Z MERAKI/Test/Github.js
+++ Z MERAKI/Test/Github.js
@@ -23,9 +23,15 @@ const endpoint_c9 =
 getAllMembers(endpoint_c9, token_c9);
 */
 
-const isAMember = (endpoint, token, REQUEST_TYPE = "get") => {
-  console.log("a", token);
-  console.log("b", endpoint);
+const GitHubMainAPI = "https://api.github.com";
+
+const isAMember = (
+  studentGitHubUsername,
+  token,
+  cohortORG,
+  REQUEST_TYPE = "get"
+) => {
+  // console.log("a", token);
 
   var options = {
     method: REQUEST_TYPE,
@@ -34,18 +40,35 @@ const isAMember = (endpoint, token, REQUEST_TYPE = "get") => {
     // 'payload' : formData
   };
 
+  const endpoint = `${GitHubMainAPI}/orgs/${cohortORG}/members/${studentGitHubUsername}`;
+  // console.log("b", endpoint);
   const res = UrlFetchApp.fetch(endpoint, options);
+
+  // console.log("c", res);
+  // console.log("c", res.getResponseCode());
   Logger.log(res.getContentText());
+
+  const result = res.getResponseCode() == 204 ? true : false;
+  // console.log("z", result);
+  return result;
 };
-
-const token_c9 =
-  "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";
-
-const endpoint_c9 =
-  "https://api.github.com/orgs/MERAKI-Academy-Cohort-9/members";
-
-isAMember(endpoint_c9, token_c9);
 
 const doneFunc = () => {
   console.log("DONE");
 };
+
+// ================== TEST Section
+
+const token_c9 =
+  "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";
+
+let testStudents = "MERAKI-Academy";
+testStudents = "Huda";
+testStudents = "Alaaalnseirat";
+testStudents = "AyaAlomarii";
+
+const c9ORG = "MERAKI-Academy-Cohort-9";
+
+// isAMember(testStudents, token_c9, c9ORG);
+
+// ! - start section 2
